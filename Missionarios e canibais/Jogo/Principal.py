237977cd8627ed386bb9275f8a035b6987d30145
Matheus Polidoro <matheus.polidoro@senaisp.edu.br>
2021-09-10 12:41:03 -0300

    Versão com Pygame

--- Missionarios e canibais/Jogo/Principal.py
+++ Missionarios e canibais/Jogo/Principal.py
@@ -1,17 +1,18 @@
 import Jogo_2 as mc
-plataformas = {'dC': 3, 'dM': 3, 'eC': 0, 'eM': 0, 'C': 0, 'M': 0}
-lados = {'0': 'd', '1': 'e', '2': 'd'}
-comeu = False
-while plataformas['eC'] + plataformas['eM'] != 6:
-    for lado in range(0,2):
-        if comeu == False:
-            mc.Cabecalho(lados[str(lado)],plataformas)
-            mc.SobePassageiro(lados[str(lado)], plataformas)
-            mc.DescePassageiro(lados[str(lado + 1)], plataformas)
-            comeu = mc.ValidaStatus(plataformas,comeu)
-        else:
+def main():
+    plataformas = {'dC': 3, 'dM': 3, 'eC': 0, 'eM': 0, 'C': 0, 'M': 0}
+    lados = {'0': 'd', '1': 'e', '2': 'd'}
+    comeu = False
+    while plataformas['eC'] + plataformas['eM'] != 6:
+        for lado in range(0,2):
+            if comeu == False:
+                mc.Cabecalho(lados[str(lado)],plataformas)
+                mc.SobePassageiro(lados[str(lado)], plataformas)
+                mc.DescePassageiro(lados[str(lado + 1)], plataformas)
+                comeu = mc.ValidaStatus(plataformas,comeu)
+            else:
+                break
+        if comeu == True:
             break
-    if comeu == True:
-        break
-mc.Final(comeu,lado, plataformas)
+    mc.Final(comeu,lado, plataformas)
 
